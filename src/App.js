a6e5cc8c971f8803fd3d005e51440a0241a98f80
Havrick <asrahdr@gmail.com>
2025-02-09 15:47:36 +0100

    Random quote machine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css'; // CSS file style files
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Bootstrap ICONS
+
+import React, { useState } from 'react';
+
+import {famousQuotes} from './quotes.js';
+import {colors} from './colors.js';
+
+function App(){
+  const [quote, setQuote] = useState(famousQuotes[Math.floor(Math.random() * famousQuotes.length)]);
+  const [color, setColor] = useState(colors[Math.floor(Math.random() * colors.length)]);
+  function newQuote(){
+    setQuote( () => famousQuotes[Math.floor(Math.random() * famousQuotes.length)]);
+    setColor( () => colors[Math.floor(Math.random() * colors.length)]);
+  }
+  
+  const messageToShare = `${quote.quote}" - ${quote.author}`;
+  
+  const backgroundStyle = {
+    backgroundColor: color
+  };
+  const buttonStyle = {
+    color: '#F5F5F5',
+    backgroundColor: color
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="d-flex justify-content-center align-items-center vw-100 animation-background mybackground" style={backgroundStyle}>
+        <div id='quote-box' className='align-items-center bg-light p-4 container-sm rounded content' style={{color: color}}>
+          <p id='text' className='text-center fs-2 m-5'>
+            <i className="bi bi-quote"></i>
+            {quote.quote}
+            <i className="bi bi-quote"></i>
+          </p>
+          <p id='author' className='text-end'>- {quote.author}</p>
+          <div className='d-flex flex-row justify-content-between'>
+            <button id='new-quote' className='btn' onClick={newQuote} style={buttonStyle} >New quote</button>
+            <div className='d-flex justify-content-md-between'>
+              <a 
+                href={`twitter.com/intent/tweet?hashtags=quotes&text="${messageToShare}"`} 
+                target="_blank" 
+                id='tweet-quote' 
+                className='btn fs-3 text-center mx-1' 
+                style={buttonStyle}
+                rel="noreferrer"
+                ><i className="bi bi-twitter-x"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className="d-flex align-items-center"> 
+        <div className="container text-center ">
+          <div className="row">
+            <div className="col d-flex align-items-center justify-content-center">
+              <span>Miguel Costa 2025</span>
+              </div>
+              <div className="col">
+                <a href="https://github.com/MAFernCosta" className="link-light link-underline-opacity-0"><i class="bi bi-github fs-4"></i></a> 
+                <a href="https://maferncosta.github.io/" className="link-light link-underline-opacity-0"><i class="bi bi-house-door-fill fs-4"></i></a>
+                </div>
+              </div>           
+        </div>
+      </footer>
+    </>
+  )
 }
 
 export default App;
---
+++ src/colors.js
@@ -0,0 +1,27 @@
+export const colors =[
+    "#2A3D5C", // Deep slate blue
+    "#2E4053", // Charcoal blue
+    "#3C6E71", // Deep teal green
+    "#3A4C5C", // Dark blue-grey
+    "#355C7D", // Cool steel blue
+    "#4F6272", // Muted slate grey
+    "#5C6D7C", // Soft navy
+    "#4A6572", // Dusty blue
+    "#3E5C6A", // Steel teal
+    "#2F4F4F",  // Dark sea green
+    "#1E1E1E", // Almost black
+    "#2C3E50", // Dark blue-gray
+    "#34495E", // Muted navy
+    "#8E44AD", // Rich purple
+    "#C0392B", // Strong red
+    "#D35400", // Deep orange
+    "#16A085", // Dark teal
+    "#27AE60", // Deep green
+    "#2980B9", // Strong blue
+    "#F39C12", // Bold yellow-orange
+    "#E67E22", // Burnt orange
+    "#8B0000", // Dark red
+    "#004953", // Deep cyan
+    "#3D3B8E", // Strong indigo
+    "#5D4157", // Deep mauve
+  ];
